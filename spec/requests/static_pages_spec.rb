0e660ea24b04257f0ad45993e87f35090b3209cf
Your Name <you@example.com>
2014-03-17 22:58:42 +0100

    removing duplication from tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
 
     it "should have the content 'Sample App'" do
@@ -18,7 +20,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
  
     it "should have the content 'Help'" do
@@ -36,14 +38,14 @@ describe "StaticPages" do
 
      it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
 
   describe "Contact page" do
     it "should have the right title" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
 
     it "should have the content 'Contact'" do
