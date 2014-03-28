b98b5b63e9f390edc38f56c515e9af2eb79757cb
Your Name <you@example.com>
2014-03-28 23:41:11 +0100

    adding a user signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 SampleApp::Application.routes.draw do
 
+get "users/new"
+
 root 'static_pages#home'
-match '/help', to: 'static_pages#help', via: 'get'
-match '/about', to: 'static_pages#about', via: 'get'
-match '/contact', to: 'static_pages#contact', via: 'get'
+match '/help',            to: 'static_pages#help',    via: 'get'
+match '/about',           to: 'static_pages#about',   via: 'get'
+match '/contact',         to: 'static_pages#contact', via: 'get'
+match '/signup',          to: 'users#new',            via: 'get'
 
 #  get "static_pages/home"
 #  get "static_pages/help"
