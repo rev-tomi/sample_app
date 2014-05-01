ad9242ce63c7752307c55f2734dcda7ae405e122
Your Name <you@example.com>
2014-05-02 00:29:58 +0200

    connecting login with editing

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -10,7 +10,7 @@ def sign_in(user, options={})
   if options[:no_capybara]
     remember_token = User.new_remember_token
     cookies[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.digest(remember_token))
+    user.update_attribute(:remember_token, User.hash(remember_token))
   else
     visit signin_path
     fill_in "Email",    with: user.email.upcase
