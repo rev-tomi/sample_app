1d7d56d391b5584bf9abd7c16d89cc31ffb6bb1c
Your Name <you@example.com>
2014-05-03 00:14:12 +0200

    deleting users

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -86,7 +86,18 @@ describe "Authentication" do
         before { get edit_user_path(wrong_user) }
         specify { expect(response).to redirect_to(root_url) }
       end
+    end
 
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin, no_capybara: true }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
     end
   end
 
